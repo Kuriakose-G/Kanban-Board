cfe70fe9d889003a6e2adf81d8a2bc1ca09f677b
Kuriakose George <kuriakose00@gmail.com>
2024-07-09 07:18:33 +0530

    module, route setup completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const kanbanModule = () => import('./Modules/kanban/kanban.module').then(x => x.KanbanModule);
+const planModule = () => import('./Modules/plan/plan.module').then(x => x.PlanModule);
+const scheduleModule = () => import('./Modules/schedules/schedules.module').then(x => x.SchedulesModule);
+const settingsModule = () => import('./Modules/settings/settings.module').then(x => x.SettingsModule);
+
+export const routes: Routes = [
+    { path: '', loadChildren: kanbanModule },
+    { path: 'plan', loadChildren:planModule },
+    { path: 'schedules', loadChildren:scheduleModule },
+    { path: 'settings', loadChildren:settingsModule }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {
+        initialNavigation: 'enabledBlocking',
+        scrollPositionRestoration: 'enabled'
+    })],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
